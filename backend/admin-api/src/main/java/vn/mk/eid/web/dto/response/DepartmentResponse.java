d6c5668881d4f6d26bf3ecfed0b4ad30baa9dbf5
Phuong-Chu <phuongchu404@gmail.com>
2025-09-03 17:32:51 +0700

    update

---
+++ backend/admin-api/src/main/java/vn/mk/eid/web/dto/response/DepartmentResponse.java
@@ -0,0 +1,12 @@
+package vn.mk.eid.web.dto.response;
+
+import lombok.Data;
+
+@Data
+public class DepartmentResponse {
+    private Integer id;
+
+    private String name;
+
+    private String code;
+}
